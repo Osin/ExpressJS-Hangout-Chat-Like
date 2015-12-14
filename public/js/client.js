d3d88655d58991ad2df72b1710fc777f46ceac48
housseini toumani <osin@live.com>
2015-12-14 23:22:10 +0100

    ajout de fichier

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,5 @@
-$('#btn-input').
+var socket = io.connect('http://localhost:8080');
+var username = prompt('Quel est votre nom ?');
+if(username !== null){
+    socket.emit('set_username', username);
+}
--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 var express = require('express'),
     app = express(),
-    twig = require('twig');
+    server = require('http').createServer(app),
+    twig = require('twig'),
+    http = require('http').Server(app),
+    io = require('socket.io').listen(server),
+    session = require('express-session');
 
-app.use('/static', express.static('public'));
+var test = 1;
+app.use('/static', express.static('public'))
+    .use(session({
+            secret: "I love cat",
+            resave: false,
+            saveUninitialized: false
+        }
+    ))
+    .use(function (req, res, next) {
+        var users = req.session.users;
+        if (!users) {
+            users = req.session.users = {}
+        }
+        next();
+    });
 
-app.get('/', function(req, res){
-   res.render('index.twig');
+
+// Quand on client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+        console.log('Un client est connecté !');
+        console.log('session' + session.username);
+    socket.on('set_username', function (username) {
+        session.username = username;
+        });
+    });
+
+
+app.get('/', function (req, res) {
+    console.log(req.session);
+    res.render('index.twig');
+    test++;
+    console.log(test);
 });
-app.listen(8080);
+server.listen(8080);
